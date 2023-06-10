a59ed9e89f3758a69c80fb99f65f14bb6ca71c27
frank <frankosei70@yahoo.com>
2023-06-09 23:30:45 -0700

    filled out the index.ejs

--- express_server.js
+++ express_server.js
@@ -10,14 +10,20 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
+  });
+  app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
